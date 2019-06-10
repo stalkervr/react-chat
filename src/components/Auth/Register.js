1479ff2c8df36ba55aff0c8862c29869d3296ca4
stalkervr <stalkervr@gmail.com>
2019-06-10 22:52:22 +0700

    Clear Errors upon Registration,Show loading state

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom';
 import firebase from '../../firebase';
 
 class Register extends Component {
+
         state = {
           username: '',
           email: '',
           password: '',
           passwordConfirmation: '',
-          errors: []
+          errors: [],
+          loading: false
         };
 
         isFormValid = () => {
@@ -17,14 +19,16 @@ class Register extends Component {
             let error;
         
             if (this.isFormEmpty(this.state)) {
-              error = { message: 'Fill in all fields sss1' };
+              error = { message: 'Fill in all fields !' };
               this.setState({ errors: errors.concat(error) });
               return false;
-            } else if (!this.isPasswordValid(this.state)) {
-              error = { message: 'Password is invalid sss1' };
+            } 
+            else if (!this.isPasswordValid(this.state)) {
+              error = { message: 'Password is invalid !' };
               this.setState({ errors: errors.concat(error) });
               return false;
-            } else {
+            } 
+            else {
               return true;
             }
           };
@@ -69,20 +73,29 @@ class Register extends Component {
     handleSubmit = (event) => {
         if (this.isFormValid()) {
         event.preventDefault();
+        this.setState({errors: [], loading: true});
         firebase
         .auth()
         .createUserWithEmailAndPassword(this.state.email, this.state.password)
         .then(createdUser => {
             console.log(createdUser);
+            this.setState({ loading: false});
         })
         .catch(err => {
             console.error(err.message);
+            this.setState({errors: this.state.errors.concat(err), loading: false});
         });}
     };
 
+    hendleInputError = (errors, inputName) => {
+        return (
+            errors.some(error => error.message.toLowerCase().includes(inputName)) ? 'error' : ''
+        );
+    }
+
     render() {
 
-        const { username, email, password, passwordConfirmation, errors} = this.state;
+        const { username, email, password, passwordConfirmation, errors, loading} = this.state;
 
         return (
             <Grid textAlign = 'center' verticalAlign = 'middle' className="app">
@@ -100,24 +113,28 @@ class Register extends Component {
 
                             <Form.Input fluid name="email" icon="mail" iconPosition="left" 
                             placeholder="Email" onChange={this.handleChange}
-                            value={email} 
+                            value={email}
+                            // change className atribut if have error in state
+                            className={ this.hendleInputError(errors, 'email') }
                             type="email"/>
 
                             <Form.Input fluid name="password" icon="lock" iconPosition="left" 
                             placeholder="Password" onChange={this.handleChange}
+                            className={ this.hendleInputError(errors, 'password') }
                             value={password} 
                             type="password"/>
 
                             <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" 
                             placeholder="Password confirmation" onChange={this.handleChange}
+                            className={ this.hendleInputError(errors, 'password') }
                             value={passwordConfirmation} 
                             type="password"/>
 
-                            <Button color="orange" fluid size="large">Submit</Button>
+                            <Button disabled={ loading } className={ loading ? 'loading' : ''} color="orange" fluid size="large">Submit</Button>
                         </Segment>
                     </Form>
                     {errors.length > 0 && (
-                        <Message error>
+                        <Message color='orange' error>
                             <h3>Error</h3>
                             {this.displayErrors(errors)}
                         </Message>
