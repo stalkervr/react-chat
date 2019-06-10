dc399decae6e1d98857127dd0b801989d52101a7
stalkervr <stalkervr@gmail.com>
2019-06-10 18:19:58 +0700

    Form Validation for Register Form

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -4,19 +4,70 @@ import { Link } from 'react-router-dom';
 import firebase from '../../firebase';
 
 class Register extends Component {
+        state = {
+          username: '',
+          email: '',
+          password: '',
+          passwordConfirmation: '',
+          errors: []
+        };
 
-    state = {
-        username: '',
-        email: '',
-        password: '',
-        passwordConfirmation: ''
-    }
+        isFormValid = () => {
+            let errors = [];
+            let error;
+        
+            if (this.isFormEmpty(this.state)) {
+              error = { message: 'Fill in all fields sss1' };
+              this.setState({ errors: errors.concat(error) });
+              return false;
+            } else if (!this.isPasswordValid(this.state)) {
+              error = { message: 'Password is invalid sss1' };
+              this.setState({ errors: errors.concat(error) });
+              return false;
+            } else {
+              return true;
+            }
+          };
+//TODO: DONT ANDERSTAND
 
-    handleChange = event => {
+/* #region   search error*/
+//   isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+//       return (
+//           !username.lenght ||
+//           !email.lenght ||
+//           !password.lenght ||
+//           !passwordConfirmation.lenght
+//       );
+//   };
+/* #endregion */
+
+        isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+        return (
+            !username.length ||
+            !email.length ||
+            !password.length ||
+            !passwordConfirmation.length
+            );
+        };
+
+    isPasswordValid = ({ password, passwordConfirmation }) => {
+        if  ( password.lenght < 6 || passwordConfirmation.lenght < 6) {
+            return false;
+        } else if ( password !== passwordConfirmation ){
+            return false;
+        } else {
+            return true;
+        }
+    };
+
+    displayErrors = errors => errors.map((error, i) => <p key={i}>{error.message}</p>)
+
+    handleChange = (event) => {
         this.setState({ [event.target.name]: event.target.value });
-    }
+    };
 
     handleSubmit = (event) => {
+        if (this.isFormValid()) {
         event.preventDefault();
         firebase
         .auth()
@@ -26,12 +77,12 @@ class Register extends Component {
         })
         .catch(err => {
             console.error(err.message);
-        });
+        });}
     };
 
     render() {
 
-        const { username, email, password, passwordConfirmation} = this.state;
+        const { username, email, password, passwordConfirmation, errors} = this.state;
 
         return (
             <Grid textAlign = 'center' verticalAlign = 'middle' className="app">
@@ -65,6 +116,12 @@ class Register extends Component {
                             <Button color="orange" fluid size="large">Submit</Button>
                         </Segment>
                     </Form>
+                    {errors.length > 0 && (
+                        <Message error>
+                            <h3>Error</h3>
+                            {this.displayErrors(errors)}
+                        </Message>
+                    )}
                     <Message>Already a user ? <Link to='/login'> Login </Link> </Message>
                 </Grid.Column>
             </Grid>
